6f9ca0a1bc8069c6acf534a55c307ffc63b9481d
devjhwan <143351063+devjhwan@users.noreply.github.com>
2024-04-23 14:31:23 +0200

    그리드 레이아웃 리스터 함수 분리

--- app/src/main/java/com/example/motel_kiosk/MainActivity.kt
+++ app/src/main/java/com/example/motel_kiosk/MainActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import androidx.annotation.RequiresApi
+import androidx.gridlayout.widget.GridLayout
 import com.example.motel_kiosk.databinding.ReserveActivityBinding
 
 
@@ -20,28 +21,39 @@ class MainActivity : ComponentActivity() {
 
         val gridLayout = binding.reserveGrid
         val buttonStates = Array(gridLayout.rowCount) { Array(gridLayout.columnCount) { false } }
+
+        setGridLayoutListener(gridLayout, buttonStates);
+    }
+
+    private fun setGridLayoutListener(gridLayout: GridLayout,
+                                      buttonStates: Array<Array<Boolean>>) {
         for (row in 0 until gridLayout.rowCount) {
             for (col in 0 until gridLayout.columnCount) {
                 val view = gridLayout.getChildAt(row * gridLayout.rowCount + col)
-                if (view is Button) {
-                    view.setOnClickListener {
-                        @RequiresApi(Build.VERSION_CODES.Q)
-                        if (!buttonStates[row][col]) // 0xDC4242
-                            view.background.colorFilter =
-                                BlendModeColorFilter(
-                                    resources.getColor(R.color.full_red, null),
-                                    BlendMode.MULTIPLY
-                                )
-                        else // 0x00BCD4
-                            view.background.colorFilter =
-                                BlendModeColorFilter(
-                                    resources.getColor(R.color.empty_blue, null),
-                                    BlendMode.MULTIPLY
-                                )
-                        buttonStates[row][col] = !buttonStates[row][col]
-                    }
-                }
+                if (view is Button)
+                    insertButtonClickListener(view, buttonStates, row, col);
             }
+        }
+    }
+
+    private fun insertButtonClickListener(button: Button,
+                                  buttonStates: Array<Array<Boolean>>,
+                                  row: Int, col: Int) {
+        button.setOnClickListener {
+            @RequiresApi(Build.VERSION_CODES.Q)
+            if (!buttonStates[row][col]) // 0xDC4242
+                button.background.colorFilter =
+                    BlendModeColorFilter(
+                        resources.getColor(R.color.full_red, null),
+                        BlendMode.MULTIPLY
+                    )
+            else // 0x00BCD4
+                button.background.colorFilter =
+                    BlendModeColorFilter(
+                        resources.getColor(R.color.empty_blue, null),
+                        BlendMode.MULTIPLY
+                    )
+            buttonStates[row][col] = !buttonStates[row][col]
         }
     }
 }
